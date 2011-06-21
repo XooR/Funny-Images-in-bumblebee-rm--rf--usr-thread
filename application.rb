c83796a69859c8fe4b7d0f72cc9f70ced8e412c1
kinopyo <fantasyday@gmail.com>
2011-06-21 17:19:39 +0900

    apply cache

--- application.rb
+++ application.rb
@@ -2,35 +2,51 @@ require "sinatra"
 require 'nokogiri'
 require 'net/http' 
 require 'net/https'
+require 'dalli'
+
+# cache 6 hours
+cache = Dalli::Client.new(nil, {:expires_in => 60*60*6})
+set :cache, cache
+
 
 get '/' do
+
   begin
-    url = "https://github.com/MrMEEE/bumblebee/commit/a047be85247755cdbe0acce6f1dafc8beb84f2ac"
-    url = URI.parse( url )
-    http = Net::HTTP.new( url.host, url.port )
-    http.use_ssl = true if url.port == 443
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE if url.port == 443
-    # path = url.path + "?" + url.query
-    path = url.path
-    res, data = http.get( path )
+    @image_urls = settings.cache.get('urls')
+    @time = settings.cache.get('time')
+    
+    if @image_urls.nil?
+      url = "https://github.com/MrMEEE/bumblebee/commit/a047be85247755cdbe0acce6f1dafc8beb84f2ac"
+      url = URI.parse( url )
+      http = Net::HTTP.new( url.host, url.port )
+      http.use_ssl = true if url.port == 443
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE if url.port == 443
+      # path = url.path + "?" + url.query
+      path = url.path
+      res, data = http.get( path )
 
-    @image_urls = Array.new
+      @image_urls = Array.new
 
-    case res
-      when Net::HTTPSuccess, Net::HTTPRedirection
-        # parse link
-        doc = Nokogiri::HTML(data)
-        doc.css('div.comment').each do |comment|
-          comment.search('div.body img').each do |img|
-            @image_urls << img["src"]
+      case res
+        when Net::HTTPSuccess, Net::HTTPRedirection
+          # parse link
+          doc = Nokogiri::HTML(data)
+          doc.css('div.comment').each do |comment|
+            comment.search('div.body img').each do |img|
+              @image_urls << img["src"]
+            end
           end
-        end
-      else
-        return "failed" + res.to_s
+          settings.cache.set('urls', @image_urls)
+          settings.cache.set('time', Time.now)
+        else
+          return "failed" + res.to_s
+      end
     end
 
+    @time = Time.now if @time.nil?
     erb :index
   rescue Exception => e
+    @message = e.message
     erb :error
   end
   
@@ -53,9 +69,11 @@ __END__
 </html>
 
 @@ index
+Image grabbed at <%=@time%>
 <% @image_urls.each do |url| %>
   <p><img src="<%= url %>"></p>
 <% end %>
 
 @@ error
 <p>Oops! Something went wrong...</p>
+<p>Detail:: <%= @message %></p>
